4c95f8d938f2800bf809e3814c4d6718be9ffdf4
PySimon25 <213518354+PySimon25@users.noreply.github.com>
2025-07-24 00:28:05 +0200

    es5 test per messaggio eccezione

--- test/test_es5_password_validator.py
+++ test/test_es5_password_validator.py
@@ -8,20 +8,24 @@ class TestPasswordValidator(unittest.TestCase):
         self.password_validator = PasswordValidator()
 
     def test_password_too_short_exception(self) -> None:
-        with self.assertRaises(PasswordTooShortError):
+        with self.assertRaises(PasswordTooShortError) as context:
             self.password_validator.validate("patate")
+        self.assertEqual(str(context.exception), "La password deve contenere almeno 8 caratteri.")
 
     def test_password_missing_uppercase_exception(self) -> None:
-        with self.assertRaises(PasswordMissingUppercaseError):
+        with self.assertRaises(PasswordMissingUppercaseError) as context:
             self.password_validator.validate("patateintecia")
+        self.assertEqual(str(context.exception), "La password deve contenere almeno una lettera maiuscola.")
 
     def test_password_missing_lowercase_exception(self) -> None:
-        with self.assertRaises(PasswordMissingLowercaseError):
+        with self.assertRaises(PasswordMissingLowercaseError) as context:
             self.password_validator.validate("PATATEINTECIA")
+        self.assertEqual(str(context.exception), "La password deve contenere almeno una lettera minuscola.")
 
     def test_password_missing_digit_exception(self) -> None:
-        with self.assertRaises(PasswordMissingDigitError):
+        with self.assertRaises(PasswordMissingDigitError) as context:
             self.password_validator.validate("PaTaTeInTeCiA")
+        self.assertEqual(str(context.exception), "La password deve contenere almeno un numero.")
 
     def test_valid_operation_no_exceptions(self) -> None:
         try:
